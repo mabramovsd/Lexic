1029bc236e62ed100c375fe85c13a458243dfb87
Mikhail Abramov <beavisabra@yandex.ru>
2020-04-28 12:56:28 +0400

    Add files via upload

---
+++ Lexic/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace Lexic
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Части
+        /// </summary>
+        Dictionary<string, string> parts = new Dictionary<string, string>();
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                parts = new Dictionary<string, string>();
+
+                int i = 0;
+                string subText = "";
+                foreach (char s in textBox1.Text + " ")
+                {
+                    if (Lexema.IsOperator(subText) &&
+                        (s == ' ' || s == '<' || s == '>' || s == ';'))
+                    {
+                        i++;
+                        parts.Add(i.ToString() + " " + subText, "Operator");
+                        subText = "";
+                    }
+                    else if (Lexema.IsArithmOperator(subText) &&
+                        (s == ' ' || s == '(' || Char.IsDigit(s) || Char.IsLetter(s)))
+                    {
+                        i++;
+                        parts.Add(i.ToString() + " " + subText, "Oper");
+                        subText = "";
+                    }
+                    else if (Lexema.IsNumber(subText) &&
+                        (s == ' ' || s == ';' || s == ')'))
+                    {
+                        i++;
+                        parts.Add(i.ToString() + " " + subText, "Number");
+                        subText = "";
+                    }
+                    else if (Lexema.CanBeVariable(subText) &&
+                        !Lexema.IsOperator(subText) &&
+                        (s == ' ' || s == '<' || s == '>' || s == ';' ||
+                        s == '+' || s == '-' || s == '*' || s == '/'))
+                    {
+                        i++;
+                        parts.Add(i.ToString() + " " + subText, "Variable");
+                        subText = "";
+                    }
+                    else if (Lexema.IsSemicolon(subText))
+                    {
+                        i++;
+                        parts.Add(i.ToString() + " " + subText, "Semicolon");
+                        subText = "";
+                    }
+                    else if (Lexema.IsIO(subText))
+                    {
+                        i++;
+                        parts.Add(i.ToString() + " " + subText, "IO");
+                        subText = "";
+                    }
+                    else if (Lexema.IsBracket(subText))
+                    {
+                        i++;
+                        parts.Add(i.ToString() + " " + subText, "Bracket");
+                        subText = "";
+                    }
+                    else if (Lexema.IsIf(subText) &&
+                        (s == ' ' || s == '('))
+                    {
+                        i++;
+                        parts.Add(i.ToString() + " " + subText, "If");
+                        subText = "";
+                    }
+                    else if (subText == Environment.NewLine || subText == " ")
+                    {
+                        subText = "";
+                    }
+
+                    subText += s;
+                }
+
+
+
+
+                textBox2.Clear();
+                foreach (KeyValuePair<string, string> pair in parts)
+                {
+                    textBox2.Text += Environment.NewLine + 
+                        pair.Key.PadRight(20) + " " + pair.Value;
+                }
+            }
+        }
+    }
+}
---
+++ Lexic/Function.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace Lexic
+{
+    class Lexema
+    {
+        /// <summary>
+        /// Оператор cin / cout
+        /// </summary>
+        public static bool IsOperator(string text)
+        {
+            return (text == "cout" || text == "cin" || text == "endl");
+        }
+
+        /// <summary>
+        /// Арифметический Оператор
+        /// </summary>
+        public static bool IsArithmOperator(string text)
+        {
+            return (text == "<" || text == ">" || text == "==" || text == "=" ||
+                    text == "+" || text == "-" || text == "*" || text == "/");
+        }
+
+        /// <summary>
+        /// Число
+        /// </summary>
+        public static bool IsNumber(string text)
+        {
+            int x;
+            return Int32.TryParse(text, out x);
+        }
+
+        /// <summary>
+        /// Похоже на переменную
+        /// </summary>
+        public static bool CanBeVariable(string text)
+        {
+            bool CanBe = true;
+
+            if (text.Length == 0 || !Char.IsLetter(text[0]))
+                CanBe = false;
+            else 
+                foreach (char s in text)
+                {
+                    if (s != '_' && !Char.IsDigit(s) && !Char.IsLetter(s))
+                        CanBe = false;
+                }
+            return CanBe;
+        }
+
+        /// <summary>
+        /// ;
+        /// </summary>
+        public static bool IsSemicolon(string text)
+        {
+            return (text == ";");
+        }
+
+        /// <summary>
+        /// Ввод-вывод
+        /// </summary>
+        public static bool IsIO(string text)
+        {
+            return (text == "<<" || text == ">>");
+        }
+        
+        /// <summary>
+        /// Скобка
+        /// </summary>
+        public static bool IsBracket(string text)
+        {
+            return (text == "(" || text == ")");
+        }
+
+        /// <summary>
+        /// Условие
+        /// </summary>
+        public static bool IsIf(string text)
+        {
+            return (text == "if" || text == "else");
+        }
+    }
+}
